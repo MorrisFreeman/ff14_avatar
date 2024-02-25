0fecaea3287b8dc8ef8205ff002cc0b936d68bfb
MorrisFreeman <morrisfreeman0718@gmail.com>
2024-02-25 11:50:14 +0900

    feat: 以下の要素も取得するように Job Achievement Minion Mount

--- examples/fetch_avatar.rs
+++ examples/fetch_avatar.rs
@@ -2,9 +2,13 @@ use ff14_avatar::fetch_avatar;
 
 #[tokio::main]
 async fn main() {
-    let id = "YOUR";
+    let id = "31270495";
     let avatar = fetch_avatar(id).await;
     println!("ID: {}", avatar.id);
     println!("Name: {}", avatar.name);
     println!("ImageUrl: {}", avatar.image_url);
+    println!("Jobs: {:?}", avatar.jobs);
+    println!("Achievements: {:?}", avatar.achievements);
+    println!("Minions: {:?}", avatar.minions);
+    println!("Mounts: {:?}", avatar.mounts);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,10 @@
 /// }
 /// ```
 use scraper::Selector;
+use regex::Regex;
+use chrono::{TimeZone, DateTime, Utc};
+use chrono_tz::{Asia::Tokyo, Tz};
+
 
 /// A FF14 character avatar.
 #[derive(Debug)]
@@ -24,6 +28,56 @@ pub struct FF14Avatar {
     pub id: String,
     pub name: String,
     pub image_url: String,
+    pub jobs: Jobs,
+    pub achievements: Achievements,
+    pub minions: Minions,
+    pub mounts: Mounts,
+}
+
+#[derive(Debug)]
+pub struct Mount {
+    name: String,
+    image_url: String,
+}
+
+#[derive(Debug)]
+pub struct Mounts {
+    items: Vec<Mount>,
+    count: i32,
+}
+
+#[derive(Debug)]
+pub struct Minions {
+    items: Vec<Minion>,
+    count: i32,
+}
+
+#[derive(Debug)]
+pub struct Minion {
+    name: String,
+    image_url: String,
+}
+
+#[derive(Debug)]
+pub struct Achievements {
+    items: Vec<Achievement>,
+    count: i32,
+}
+
+#[derive(Debug)]
+pub struct Achievement {
+    pub title: String,
+    pub date: DateTime<Tz>,
+    pub image_url: String,
+}
+
+type Jobs = Vec<Job>;
+
+#[derive(Debug)]
+pub struct Job {
+    name: String,
+    level: String,
+    image_url: String,
 }
 
 /// Fetches FF14 character avatars.
@@ -63,9 +117,111 @@ pub async fn fetch_avatar(id: &str) -> FF14Avatar {
     let href = node.value().attr("href").unwrap();
     let image_url = href.to_string();
 
+    // Get the character jobs
+    let mut jobs: Vec<Job> = Vec::new();
+    let job_sel = Selector::parse(".character__level__list li").unwrap();
+    for node in doc.select(&job_sel) {
+        let level = node.text().collect::<Vec<_>>().join("");
+        let node = node.select(&Selector::parse("img").unwrap()).next().unwrap();
+        let name = node.value().attr("data-tooltip").unwrap().to_string();
+        let image_url = node.value().attr("src").unwrap().to_string();
+        let job = Job {
+            name,
+            level,
+            image_url,
+        };
+
+        jobs.push(job);
+    }
+
+    // Get the character achievements
+    let url = format!("{}/{}/achievement/", base_url, id);
+    let html = reqwest::get(&url).await.unwrap().text().await.unwrap();
+    let doc: scraper::Html = scraper::Html::parse_document(&html);
+    let mut items: Vec<Achievement> = Vec::new();
+    let achievement_sel = Selector::parse(".entry__achievement").unwrap();
+    for node in doc.select(&achievement_sel) {
+        let img_node = node.select(&Selector::parse("img").unwrap()).next().unwrap();
+        let image_url = img_node.value().attr("src").unwrap().to_string();
+        let date_node = node.select(&Selector::parse(".entry__activity__time script").unwrap()).next().unwrap();
+        let date_script = date_node.text().collect::<Vec<_>>().join("");
+        let re = Regex::new(r"ldst_strftime\((\d+), 'YMD'\)").unwrap();
+        let timestamp = re.captures(&date_script).unwrap().get(1).unwrap().as_str().parse().unwrap(); // TODO: エラーになるかもしれない
+        let datetime_utc = Utc.timestamp_opt(timestamp, 0).unwrap();
+        let date: chrono::prelude::DateTime<_> = datetime_utc.with_timezone(&Tokyo);
+        let title_node = node.select(&Selector::parse(".entry__activity__txt").unwrap()).next().unwrap();
+        let title = title_node.text().collect::<Vec<_>>().join("");
+        let achievement = Achievement {
+            title,
+            date,
+            image_url,
+        };
+
+        items.push(achievement);
+    }
+    let count_sel = Selector::parse(".parts__total").unwrap();
+    let count_str  = doc.select(&count_sel).next().unwrap().text().collect::<Vec<_>>().join("");
+    let re = Regex::new(r"(\d+).*").unwrap();
+    let count = re.captures(&count_str).unwrap().get(1).unwrap().as_str().parse().unwrap(); // TODO: エラーになるかもしれない
+
+    let achievements = Achievements {
+        items,
+        count,
+    };
+
+    // Get the character minions
+    let url = format!("{}/{}/minion/", base_url, id);
+    let html = reqwest::get(&url).await.unwrap().text().await.unwrap();
+    let doc: scraper::Html = scraper::Html::parse_document(&html);
+    let mut items: Vec<Minion> = Vec::new();
+    let minion_sel = Selector::parse(".minion__list_icon").unwrap();
+    for node in doc.select(&minion_sel) {
+        let img_node = node.select(&Selector::parse("img").unwrap()).next().unwrap();
+        let image_url = img_node.value().attr("src").unwrap().to_string();
+        let minion = Minion {
+            name: "".to_string(),
+            image_url,
+        };
+
+        items.push(minion);
+    }
+    let count_sel = Selector::parse(".minion__sort__total span").unwrap();
+    let count = doc.select(&count_sel).next().unwrap().text().collect::<Vec<_>>().join("").parse().unwrap();
+    let minions = Minions {
+        items,
+        count,
+    };
+
+    // Get the character mounts
+    let url = format!("{}/{}/mount/", base_url, id);
+    let html = reqwest::get(&url).await.unwrap().text().await.unwrap();
+    let doc: scraper::Html = scraper::Html::parse_document(&html);
+    let mut items: Vec<Mount> = Vec::new();
+    let mount_sel = Selector::parse(".character__item_icon").unwrap();
+    for node in doc.select(&mount_sel) {
+        let img_node = node.select(&Selector::parse("img").unwrap()).next().unwrap();
+        let image_url = img_node.value().attr("src").unwrap().to_string();
+        let mount = Mount {
+            name: "".to_string(),
+            image_url,
+        };
+
+        items.push(mount);
+    }
+    let count_sel = Selector::parse(".minion__sort__total span").unwrap();
+    let count = doc.select(&count_sel).next().unwrap().text().collect::<Vec<_>>().join("").parse().unwrap();
+    let mounts = Mounts {
+        items,
+        count,
+    };
+
     FF14Avatar {
         id: id.to_string(),
         name,
-        image_url
+        image_url,
+        jobs,
+        achievements,
+        minions,
+        mounts,
     }
 }
